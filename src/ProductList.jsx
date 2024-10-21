6b0c226af47f9b4913e524f1de84c1a95c723ac1
Sam-Thomas314 <Samuelajt@gmail.com>
2024-10-21 17:07:14 -0400

    Lots of changes figuring it out

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,52 +1,22 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import {useDispatch } from 'react-redux';
+import {incrementrepellantQuantitity, decrementrepellantQuantity} from './purifyingSlice';
+import {incrementpurifyingQuantity, decrementpurifyingQuantity} from './purifyingSlice';
+
+
+
+import { useSelector } from 'react-redux';
 function ProductList() {
+    const purifyingItems  = useSelector((state) => state.purifying);
+    const repellantItems  = useSelector((state) => state.repellant);
+    const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     //Hello There
     const plantsArray = [
-        {
-            category: "Air Purifying Plants",
-            plants: [
-                {
-                    name: "Snake Plant",
-                    image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
-                    description: "Produces oxygen at night, improving air quality.",
-                    cost: "$15"
-                },
-                {
-                    name: "Spider Plant",
-                    image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg",
-                    description: "Filters formaldehyde and xylene from the air.",
-                    cost: "$12"
-                },
-                {
-                    name: "Peace Lily",
-                    image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lilies-4269365_1280.jpg",
-                    description: "Removes mold spores and purifies the air.",
-                    cost: "$18"
-                },
-                {
-                    name: "Boston Fern",
-                    image: "https://cdn.pixabay.com/photo/2020/04/30/19/52/boston-fern-5114414_1280.jpg",
-                    description: "Adds humidity to the air and removes toxins.",
-                    cost: "$20"
-                },
-                {
-                    name: "Rubber Plant",
-                    image: "https://cdn.pixabay.com/photo/2020/02/15/11/49/flower-4850729_1280.jpg",
-                    description: "Easy to care for and effective at removing toxins.",
-                    cost: "$17"
-                },
-                {
-                    name: "Aloe Vera",
-                    image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
-                    description: "Purifies the air and has healing properties for skin.",
-                    cost: "$14"
-                }
-            ]
-        },
+        
         {
             category: "Aromatic Fragrant Plants",
             plants: [
@@ -88,47 +58,7 @@ function ProductList() {
                 }
             ]
         },
-        {
-            category: "Insect Repellent Plants",
-            plants: [
-                {
-                    name: "oregano",
-                    image: "https://cdn.pixabay.com/photo/2015/05/30/21/20/oregano-790702_1280.jpg",
-                    description: "The oregano plants contains compounds that can deter certain insects.",
-                    cost: "$10"
-                },
-                {
-                    name: "Marigold",
-                    image:"https://cdn.pixabay.com/photo/2022/02/22/05/45/marigold-7028063_1280.jpg",
-                    description: "Natural insect repellent, also adds color to the garden.",
-                    cost: "$8"
-                },
-                {
-                    name: "Geraniums",
-                    image: "https://cdn.pixabay.com/photo/2012/04/26/21/51/flowerpot-43270_1280.jpg",
-                    description: "Known for their insect-repelling properties while adding a pleasant scent.",
-                    cost: "$20"
-                },
-                {
-                    name: "Basil",
-                    image: "https://cdn.pixabay.com/photo/2016/07/24/20/48/tulsi-1539181_1280.jpg",
-                    description: "Repels flies and mosquitoes, also used in cooking.",
-                    cost: "$9"
-                },
-                {
-                    name: "Lavender",
-                    image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    description: "Calming scent, used in aromatherapy.",
-                    cost: "$20"
-                },
-                {
-                    name: "Catnip",
-                    image: "https://cdn.pixabay.com/photo/2015/07/02/21/55/cat-829681_1280.jpg",
-                    description: "Repels mosquitoes and attracts cats.",
-                    cost: "$13"
-                }
-            ]
-        },
+        
         {
             category: "Medicinal Plants",
             plants: [
@@ -246,6 +176,18 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+  const handeIncrementrepellantQuantity = (index) => {
+    dispatchEvent(incrementrepellantQuantitity(index));
+  }
+  const handleDecrementrepellantQuantity = (index) => {
+    dispatch(decrementrepellantQuantity(index));
+  }
+  const handeIncrementpurifyingQuantity = (index) => {
+    dispatchEvent(incrementpurifyingQuantity(index));
+  }
+  const handleDecrementpurifyingQuantity = (index) => {
+    dispatch(decrementpurifyingQuantity(index));
+  }
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -268,6 +210,48 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
+            <div className='text'>
+                <h1> Air Purifying Plants</h1>
+            </div>
+            <div className='purifying-section'>
+                {purifyingItems.map((item, index) => (
+                    <div key = {index}>
+                        <h1><div> {item.name}</div></h1>
+                            <div className=' product-card' key = {plantIndex}>
+                                <img className='product-image' src={plant.image} alt ={plant.name} />
+                                <div className='product-title'> {plant.name}</div>
+                                <div className="text">{plant.description}</div>
+                                <div>${plant.cost}</div>
+                                <button className='product-button' onClick={ () => handleAddToCart(plant)}>Add to Cart </button> 
+                            </div>
+                    </div>   
+                ))}
+            </div>
+
+
+            <div id="repellant" className='reppellant_container'>
+                <div className='text'>
+                    <h1> Repellant Plants </h1>
+                </div>
+                <div className='reppellant_plants'>
+                </div>
+            </div>
+
+            <div id="repellant" className='reppellant_container'>
+                <div className='text'>
+                    <h1> Repellant Plants </h1>
+                </div>
+                <div className='reppellant_plants'>
+                </div>
+            </div>
+
+            <div id="repellant" className='reppellant_container'>
+                <div className='text'>
+                    <h1> Repellant Plants </h1>
+                </div>
+                <div className='reppellant_plants'>
+                </div>
+            </div>
 
 
         </div>
--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+import purifyingReducer from './purifyingSlice';
+import repellantReducer from './repellantSlice';
+
 
  const store = configureStore({
     reducer: {
         cart: cartReducer,
+        purifying: purifyingReducer,
+        repellant: repellantReducer,
     },
 });
 export default store
